29a8a6a491e02dfcd1a12a5b25e12873216e3113
weber_v <victor.weber@epitech.eu>
2018-04-05 20:38:22 +0200

    save score

---
+++ score.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2018
+** cpp_arcade
+** File description:
+**
+*/
+
+#ifndef CPP_ARCADE_SCORE_HPP
+#define CPP_ARCADE_SCORE_HPP
+
+#define ERR_OPEN(path) "Error opening " + path
+
+#endif //CPP_ARCADE_SCORE_HPP
