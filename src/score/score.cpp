d10735fcd824ccb4e46844ad867815220f5b9029
weber_v <victor.weber@epitech.eu>
2018-04-07 00:39:07 +0200

    score update

--- src/score/score.cpp
+++ src/score/score.cpp
@@ -12,35 +12,78 @@
 #include <iostream>
 #include "score.hpp"
 
-std::vector<std::string> read_file(std::string path)
+PlayerStats::PlayerStats()
 {
+
+}
+
+PlayerStats::~PlayerStats()
+{
+
+}
+
+/*std::string PlayerStats::GetPlayerScore(std::string Game,
+					std::string PlayerName)
+{
+	for (int i = 0; i < _Stats.size(); i++) {
+		std::cout << "player: " << _Stats[i] << std::endl;
+		std::cout << "score: " <<  << std::endl; //debug
+	}
+	return _Stats;
+}*/
+
+void PlayerStats::AddGame(std::string Game)
+{
+	std::string path(SAVE_PATH + Game + SAVE_EXTENSION);
 	std::ifstream myfile(path);
 	std::string line;
 	std::regex reg("^(\\w+):(\\w*)$");
 	std::smatch match;
-	std::vector<std::string> result;
 
-	//ouvre le dossier path et stoque après ':' dans result
-	if (myfile) {
+	//ouvre le dossier path et stoque après ':' dans _stats
+	if (!myfile) {
+		std::ofstream outfile(SAVE_PATH + Game + SAVE_EXTENSION);
+		outfile.close();
+	} else {
 		while (getline(myfile, line)) {
-				if (std::regex_match(line, match, reg)) {
-					result.push_back(match[2]);
+			if (std::regex_match(line, match, reg)) {
+				_Stats[Game][match[1].str()] = match[2].str();
 			}
 		}
 		myfile.close();
-	} else
-		std::cout << ERR_OPEN(path) << std::endl;
-
-	//affiche le vecteur res
-	for (int i = 0;i < result.size(); i++){
-		std::cout << "ici: " << i << result[i] << std::endl;
 	}
-	return (result);
+}
+
+void PlayerStats::UpdatePlayerScore(std::string Game, std::string PlayerName,
+				    std::string Score)
+{
+	std::string path(SAVE_PATH + Game + SAVE_EXTENSION);
+	std::regex reg("^(\\w+):(\\w*)$");
+	std::smatch match;
+	std::map<std::string, std::string>::iterator i;
+
+	for (i = _Stats[Game]
+		.begin(); i != _Stats[Game].end(); i++) {
+		std::cout << i->first << std::endl;
+				if (i->first.compare(PlayerName) == 0) {
+					std::cout << "joueur " << PlayerName
+						  << " trouvé pour le jeu "
+						  << Game << std::endl;
+					break;
+					}
+	}
+	if (i == _Stats[Game].end())
+	std::cout << "joueur " << PlayerName << " non trouvé pour le jeu " <<  Game << std::endl;
 }
 
 int main(int ac, char **av)
 {
-	read_file(".save/pacman.log");
-	read_file(".save/snake.log");
+	PlayerStats Stats;
+	std::string Game("pacman");
+	std::string PlayerName("Rinck");
+
+	Stats.AddGame(Game);
+	Stats.UpdatePlayerScore(Game, PlayerName, "499999");
+	//read_file(".save/snake.log");
 	return 0;
 }
--- src/score/score.hpp
+++ src/score/score.hpp
@@ -8,6 +8,25 @@
 #ifndef CPP_ARCADE_SCORE_HPP
 #define CPP_ARCADE_SCORE_HPP
 
+#define SAVE_EXTENSION ".log"
+#define SAVE_PATH "./.save/"
+#define LIB_PATH "./lib/"
+#define GAMES_PATH "./games/"
+
 #define ERR_OPEN(path) "Error opening " + path
+
+class PlayerStats
+{
+	public:
+		PlayerStats();
+		~PlayerStats();
+	public:
+		void AddGame(std::string Game);
+		void GetGameStats(std::string Game, std::string PlayerName);
+		void UpdatePlayerScore(std::string Game, std::string
+		PlayerName, std::string Score);
+	private:
+		std::map<std::string, std::map<std::string, std::string>> _Stats;
+};
 
 #endif //CPP_ARCADE_SCORE_HPP
