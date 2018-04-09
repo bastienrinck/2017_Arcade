990a41eac9312eabf3fd200b0927a5552134c5f7
Bastien <bastien.rinck@epitech.eu>
2018-04-09 13:46:50 +0200

    fix return 84, everything was alredy set up, mistake caused by subject

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,20 @@
 
 int main(int ac, char **av)
 {
-	if (ac != 2)
+	int ret = 0;
+
+	if (ac != 2){
 		std::cerr << "Usage: " << av[0] + 2
 			<< " [Graphical library name from ./lib]" << std::endl;
+		ret = 84;
+	}
 	else {
 		srand(static_cast<unsigned int>(time(nullptr)));
 		Arcade::Core core(av[1]);
 		if (core.getLibIdx() != UINT_MAX)
 			core.start();
+		else
+			ret = 84;
 	}
-	return 0;
+	return ret;
 }
